356f756f43d6612dc0296e0d4d895437781d8455
Ricardo Bettarelli <ricardo.bettarelli@gmail.com>
2023-09-27 23:59:24 -0300

    Ações do carrinho

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -1,8 +1,22 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import cartActions from "../store/actions/cart"
+
 
 const Cart = () => {
   const cart = useSelector((state) => state.cart);
+  const product = cart.Cart
+  const dispatch  = useDispatch()
+
+  let totalPrice = 0
+
+  if(product.length > 0) {
+  for (let i = 0; i < product.length; i++) {
+
+        totalPrice += product[i].price * product[i].quantity 
+    
+    }
+}
 
   return (
     <>
@@ -59,9 +73,9 @@ const Cart = () => {
        
                     {cart.Cart.map(item => {
                         return (
-                            <tr>
+                            <tr key={item.id}>
                             <th>
-                      <button className="badge bg-danger">
+                      <button onClick={() => dispatch(cartActions.DeleteItem(cart, item))} className="badge bg-danger">
                         <i className="fas fa-window-close"></i>
                       </button>
                     </th>
@@ -78,12 +92,12 @@ const Cart = () => {
                     </th>
                     <th>R$ {item.price.toFixed(2)}</th>
                     <th>
-                      <button className="badge badge-pill bg-primary">
+                      <button onClick={() => dispatch(cartActions.AddItem(cart, item))} className="badge badge-pill bg-primary">
                         <i className="fas fa-plus"></i>
                     </button>
                     </th>
                     <th>
-                      <button className="badge badge-pill bg-danger">
+                      <button onClick={() => dispatch(cartActions.RemoveItem(cart, item))}className="badge badge-pill bg-danger">
                         <i className="fas fa-minus"></i>
                       </button>
                     </th>
@@ -96,7 +110,7 @@ const Cart = () => {
                       Total
                     </th>
                     <th colSpan="3">{cart.value} itens</th>
-                    <th colSpan="2">R$ 200.00</th>
+                    <th colSpan="2">R$ {totalPrice}</th>
                   </tr>
                 </tbody>
               </table>
--- src/components/store/actions/cart.js
+++ src/components/store/actions/cart.js
@@ -1,5 +1,17 @@
 const Add = (cart, product) => {
-    return {type: "ADD_TO_CART", cart, product}
-}
+  return { type: "ADD_TO_CART", cart, product };
+};
 
-export default {Add};
+const AddItem = (cart, product) => {
+  return { type: "ADD_ITEM", cart, product };
+};
+
+const RemoveItem = (cart, product) => {
+  return { type: "REMOVE_ITEM", cart, product };
+};
+
+const DeleteItem = (cart, product) => {
+  return { type: "DELETE_ITEM", cart, product };
+};
+
+export default { Add, AddItem, RemoveItem, DeleteItem };
